4a81004ef56d291cbd2a60a0ec90b3648e85ecdb
jako6857 <jako6857@elev.techcollege.dk>
2026-09-16 10:32:03 +0200

    Nogenlunde færdiggjort testimonials (tror ikke den mangler noget men det må tiden vise)
    starter nu på nyhedspage da vi allerede har logikken til at komme derind, og vise en nyhed. lavede jeg sammen med nyhedsCTA på forsiden

--- Web/src/components/Testimonials.jsx
+++ Web/src/components/Testimonials.jsx
@@ -1,25 +1,50 @@
 import { getTestimonialsData } from "../api/testimonials";
 import { useEffect, useState } from "react";
+import "../scss/Testimonials.scss";
 
 function Testimonials() {
   const [testimonials, setTestimonials] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
   useEffect(() => {
-    getTestimonialsData("testimonials")
+    getTestimonialsData("testimony")
       .then((data) => setTestimonials(data))
       .catch((error) =>
         console.error("Fejl ved hentning af testimonials:", error),
       );
   }, []);
 
+  useEffect(() => {
+    if (testimonials.length <= 1) return; // Hvis der kun er en testimonial, skal vi ikke ændre activeIndex
+
+    const timer = setInterval(() => {
+      setActiveIndex((prev) => (prev + 1) % testimonials.length);
+    }, 5000);
+    //vores timer som automatik skifter testimonial hvert 5 sekund.
+
+    //retunerer et helt clean interval på timeren samme med længden, så den altid ved hvor langt i processen den er.
+    return () => clearInterval(timer);
+  }, [testimonials.length]);
+
+  if (testimonials.length === 0) return null;
+
+  const current = testimonials[activeIndex];
+
+  //her retunerer vi bare vores view med det data vi kan få fra vores api tilhørende testimonials :) fx. current.title...
   return (
-    <section className="testimonials">
-      <div className="testimonials-container">
-        {testimonials.map((testimonial) => (
-          <div key={testimonial.id} className="testimonial">
-            <h2>{testimonial.title}</h2>
-            <p>{testimonial.text}</p>
-            <p>- {testimonial.author}</p>
-          </div>
+    <section className="testimonials container">
+      <div className="testimonial">
+        <h2 className="testimonials-title">{current.title}</h2>
+        <article className="testimonials-content">{current.content}</article>
+        <p className="testimonials-name"> {current.name}</p>
+      </div>
+
+      <div className="testimonials-dots">
+        {testimonials.map((t, index) => (
+          <button
+            key={t.id}
+            className={index === activeIndex ? "active" : ""}
+            onClick={() => setActiveIndex(index)}
+          />
         ))}
       </div>
     </section>
